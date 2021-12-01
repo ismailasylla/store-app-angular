0ba5518619e484af9b79dab7b338c919e01ba42e
ismailasylla <ismailasyllaa@gmail.com>
2021-12-01 23:32:35 +0400

    Adds, Product & button...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { ProductsComponent } from './components/products/products.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonNormalComponent } from './components/buttons/button-normal/button-normal.component';
+import { MaterialModule } from './shared/material/material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ButtonNormalComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({}, {}),
+    BrowserAnimationsModule,
+    MaterialModule
   ],
-  providers: [],
+  exports: [MaterialModule],
+  providers: [MaterialModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/buttons/button-normal/button-normal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button-normal',
+  templateUrl: './button-normal.component.html',
+  styleUrls: ['./button-normal.component.css']
+})
+export class ButtonNormalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
